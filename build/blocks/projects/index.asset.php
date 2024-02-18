93fb204d0e536ed306a8c92281dd3bf16f7cb329
Kayla Nguyen <nnguyen1@my.wctc.edu>
2024-02-18 15:43:53 -0600

    Added basic style to editor and block style

--- build/blocks/projects/index.asset.php
+++ build/blocks/projects/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '4474b18163b70c0a94ea');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'e150a512c93a720f16c8');
