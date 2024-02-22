3bfc84b775abb2d654709b1508fbe638103a37d1
Kayla Nguyen <nnguyen1@my.wctc.edu>
2024-02-22 10:54:20 -0600

    Adjust render.php to make color changes show on front end

--- src/blocks/staff-list/render.php
+++ src/blocks/staff-list/render.php
@@ -3,6 +3,12 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 
+/**
+ * @var array $attributes Attributes from the block
+ * @var string $content The HTML returned from the save() function
+ * @var WP_Block $block All the details about this instance (which post/page this block is inserted into, name of the block)
+ */
+
 $query = new WP_Query([
 			'post_type' => 'staff',
 			'orderby' => 'post_title',
@@ -19,8 +25,11 @@ $query = new WP_Query([
 			<div class="flip-card-front">
 				<?= get_the_post_thumbnail() ?>
 			</div>
-			<div class="flip-card-back">
-				<h3 class="name"><?= get_the_title() ?></h3>
+			<div class="flip-card-back"
+                 style="background-color: <?= $attributes['cardColor'] ?>;
+                         color: <?= $attributes['textColor'] ?>;"
+            >
+				<h3 class="name" style="color: <?= $attributes['headingColor'] ?>;"><?= get_the_title() ?></h3>
 				<div class="position"><?= get_post_meta(get_the_ID(), 'staff_position', true) ?></div>
 				<div class="bio">
 					<p><?= get_the_excerpt() ?></p>
