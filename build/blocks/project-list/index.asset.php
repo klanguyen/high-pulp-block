5024f7de40d1c2651fdbbdfe5f977a7bdf578498
Kayla Nguyen <nnguyen1@my.wctc.edu>
2024-02-22 23:41:44 -0600

    Added keywords to project list block

---
+++ build/blocks/project-list/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '9b718f6b3c89f60578cf');
---
+++ build/blocks/project-list/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '2ea606b887814aa703ba');
