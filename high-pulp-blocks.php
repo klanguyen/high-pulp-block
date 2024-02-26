113369c604f98c595dd1cfba2b7ede43e31e4acb
Kayla Nguyen <nnguyen1@my.wctc.edu>
2024-02-26 09:50:15 -0600

    Registered filters to high-pulp-blocks

--- high-pulp-blocks.php
+++ high-pulp-blocks.php
@@ -29,5 +29,6 @@ function high_pulp_blocks_high_pulp_blocks_block_init() {
 	register_block_type( __DIR__ . '/build/blocks/projects' );
 	register_block_type( __DIR__ . '/build/blocks/staff-list' );
 	register_block_type( __DIR__ . '/build/blocks/project-list' );
+	register_block_type( __DIR__ . '/build/filters/border-control' );
 }
 add_action( 'init', 'high_pulp_blocks_high_pulp_blocks_block_init' );
