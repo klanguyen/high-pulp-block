73b69918c5959bf927cfbba8aae70598624c2155
Kayla Nguyen <nnguyen1@my.wctc.edu>
2024-02-26 10:06:49 -0600

    Adjusted filters.php to show border on front end

--- filters.php
+++ filters.php
@@ -1,8 +1,15 @@
 <?php
 function kn_add_borders( $block_content = '', $block = [] ) {
-	// TODO: create wrapper
+	// get the attributes while setting some defaults
+	$defaults = ['borderStyle' => 'none'];
+	$attrs = array_merge($defaults, $block['attrs']);
 
-	// return unmodified block content
+	// only do this for blocks that have borders
+	if($attrs['borderStyle'] !== 'none'){
+		return '<div style="border-style: '.$attrs['borderStyle'].'">'.$block_content.'</div>';
+	}
+
+	// else, return unmodified block content
 	return $block_content;
 }
 
