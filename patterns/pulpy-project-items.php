cb16ce659ff9506bc64d12e5dcf4df791dac1fd7
Kayla Nguyen <nnguyen1@my.wctc.edu>
2024-03-04 00:51:47 -0600

    Added pulpy project items block pattern

---
+++ patterns/pulpy-project-items.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * Title: Pulpy Project Items
+ * Slug: high-pulp-blocks/pulpy-project-items
+ * Categories: featured, pulpy
+ *
+ * More headers can be found at:
+ * https://developer.wordpress.org/themes/features/block-patterns/#registering-block-patterns
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"},"metadata":{"name":"Pulpy Project Items"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center"} -->
+	<h2 class="wp-block-heading has-text-align-center">Some things I've built</h2>
+	<!-- /wp:heading -->
+
+	<!-- wp:blockart/section {"clientId":"d057e3e8","columns":3,"childRow":true} -->
+	<div class="blockart-section blockart-section-d057e3e8"><div class="blockart-container"><div class="blockart-section-inner"><!-- wp:blockart/column {"clientId":"388b3ef8","colWidth":{"desktop":33.33,"tablet":100,"mobile":100},"border":{"border":1,"radius":{"desktop":{"lock":true,"top":8,"right":8,"bottom":8,"left":8,"unit":"px"}},"size":{"desktop":{"lock":true,"top":1,"right":1,"bottom":1,"left":1,"unit":"px"}},"type":"none"},"boxShadow":{"boxShadow":1,"enable":true,"position":"outset","spread":-17,"blur":27,"horizontalX":0,"verticalY":4},"blockPadding":{"dimension":1,"desktop":{"lock":true,"unit":"px"}}} -->
+				<div class="blockart-column blockart-column-388b3ef8"><div class="blockart-column-inner"><!-- wp:blockart/image {"clientId":"ee94f3e1","image":{"url":"http://place-hold.it/300","alt":""}} -->
+						<figure class="blockart-image blockart-image-ee94f3e1"><img class="wp-image-1360 hover-effect-static filter-none mask mask-none" src="http://place-hold.it/300" alt=""/></figure>
+						<!-- /wp:blockart/image -->
+
+						<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
+						<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:heading {"level":4,"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"0"}}}} -->
+							<h4 class="wp-block-heading" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:0">TMDB </h4>
+							<!-- /wp:heading -->
+
+							<!-- wp:paragraph -->
+							<p>A movie library</p>
+							<!-- /wp:paragraph -->
+
+							<!-- wp:blockart/button {"clientId":"bf448832"} -->
+							<div class="blockart-buttons blockart-buttons-bf448832 is-wrap"><!-- wp:blockart/button-inner {"clientId":"d7d9f989","text":"HTML","size":"custom","padding":{"dimension":1,"desktop":{"lock":true,"top":10,"right":10,"bottom":10,"left":10,"unit":"px"}},"typography":{"typography":1,"size":{"desktop":{"value":12,"unit":"px"}},"weight":"300"},"color1":"#1d1d1d","hoverColor1":"#1d1d1d","background1":{"background":1,"type":"color","color":"#ffb0b0e6"},"hoverBackground1":{"background":1,"color":"#ffb0b0e6"},"border1":{"border":1,"radius":{"desktop":{"lock":true,"top":3,"right":3,"bottom":3,"left":3,"unit":"px"}},"size":{"desktop":{"lock":true}}}} -->
+								<div class="blockart-button blockart-button-d7d9f989 "><a class="blockart-button-link is-custom is-style-filled">HTML</a></div>
+								<!-- /wp:blockart/button-inner -->
+
+								<!-- wp:blockart/button-inner {"clientId":"d7d9f989","text":"CSS","size":"custom","padding":{"dimension":1,"desktop":{"lock":true,"top":10,"right":10,"bottom":10,"left":10,"unit":"px"}},"typography":{"typography":1,"size":{"desktop":{"value":14,"unit":"px"}}},"color1":"#1d1d1d","hoverColor1":"#1d1d1d","background1":{"background":1,"type":"color","color":"#ffb0b0e6"},"hoverBackground1":{"background":1,"color":"#ffb0b0e6"},"border1":{"border":1,"radius":{"desktop":{"lock":true,"top":3,"right":3,"bottom":3,"left":3,"unit":"px"}},"size":{"desktop":{"lock":true}}}} -->
+								<div class="blockart-button blockart-button-d7d9f989 "><a class="blockart-button-link is-custom is-style-filled">CSS</a></div>
+								<!-- /wp:blockart/button-inner -->
+
+								<!-- wp:blockart/button-inner {"clientId":"d7d9f989","text":"Vue.js","size":"custom","padding":{"dimension":1,"desktop":{"lock":true,"top":10,"right":10,"bottom":10,"left":10,"unit":"px"}},"typography":{"typography":1,"size":{"desktop":{"value":14,"unit":"px"}}},"color1":"#1d1d1d","hoverColor1":"#1d1d1d","background1":{"background":1,"type":"color","color":"#ffb0b0e6"},"hoverBackground1":{"background":1,"color":"#ffb0b0e6"},"border1":{"border":1,"radius":{"desktop":{"lock":true,"top":3,"right":3,"bottom":3,"left":3,"unit":"px"}},"size":{"desktop":{"lock":true}}}} -->
+								<div class="blockart-button blockart-button-d7d9f989 "><a class="blockart-button-link is-custom is-style-filled">Vue.js</a></div>
+								<!-- /wp:blockart/button-inner --></div>
+							<!-- /wp:blockart/button -->
+
+							<!-- wp:separator {"className":"is-style-wide"} -->
+							<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+							<!-- /wp:separator -->
+
+							<!-- wp:blockart/button {"clientId":"d186031b","blockMargin":{"dimension":1,"desktop":{"lock":true,"top":0,"right":0,"bottom":0,"left":0,"unit":"px"}}} -->
+							<div class="blockart-buttons blockart-buttons-d186031b is-wrap"><!-- wp:blockart/button-inner {"clientId":"81253b1c","text":"View project ","style":"plain","typography":{"typography":1,"size":{"desktop":{"value":16,"unit":"px"}},"weight":"300"},"icon":{"enable":true,"icon":"arrowRight"},"iconSize":{"desktop":{"value":24,"unit":"px"}},"iconGap":{"desktop":{"value":5,"unit":"px"}},"color2":"#000000","blockMargin":{"dimension":1,"desktop":{"lock":false,"bottom":20,"unit":"px"}}} -->
+								<div class="blockart-button blockart-button-81253b1c "><a class="blockart-button-link is-large is-style-plain">View project <span class="blockart-button-icon"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="blockart-icon" aria-hidden="true"><path d="M21.8 12l-7-6v5H2v2h12.8v5l7-6z"></path></svg></span></a></div>
+								<!-- /wp:blockart/button-inner --></div>
+							<!-- /wp:blockart/button --></div>
+						<!-- /wp:group --></div></div>
+				<!-- /wp:blockart/column -->
+
+				<!-- wp:blockart/column {"clientId":"6873fa03","colWidth":{"desktop":33.33,"tablet":100,"mobile":100},"border":{"border":1,"radius":{"desktop":{"lock":true,"top":8,"right":8,"bottom":8,"left":8,"unit":"px"}},"size":{"desktop":{"lock":true,"top":1,"right":1,"bottom":1,"left":1,"unit":"px"}},"type":"none"},"boxShadow":{"boxShadow":1,"enable":true,"position":"outset","spread":-17,"blur":27,"horizontalX":0,"verticalY":4},"blockPadding":{"dimension":1,"desktop":{"lock":true,"unit":"px"}}} -->
+				<div class="blockart-column blockart-column-6873fa03"><div class="blockart-column-inner"><!-- wp:blockart/image {"clientId":"7baa638c","image":{"url":"http://place-hold.it/300","alt":""}} -->
+						<figure class="blockart-image blockart-image-7baa638c"><img class="wp-image-1360 hover-effect-static filter-none mask mask-none" src="http://place-hold.it/300" alt=""/></figure>
+						<!-- /wp:blockart/image -->
+
+						<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
+						<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:heading {"level":4,"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"0"}}}} -->
+							<h4 class="wp-block-heading" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:0">TMDB </h4>
+							<!-- /wp:heading -->
+
+							<!-- wp:paragraph -->
+							<p>A movie library</p>
+							<!-- /wp:paragraph -->
+
+							<!-- wp:blockart/button {"clientId":"bf448832"} -->
+							<div class="blockart-buttons blockart-buttons-bf448832 is-wrap"><!-- wp:blockart/button-inner {"clientId":"d7d9f989","text":"HTML","size":"custom","padding":{"dimension":1,"desktop":{"lock":true,"top":10,"right":10,"bottom":10,"left":10,"unit":"px"}},"typography":{"typography":1,"size":{"desktop":{"value":14,"unit":"px"}}},"color1":"#1d1d1d","hoverColor1":"#1d1d1d","background1":{"background":1,"type":"color","color":"#ffb0b0e6"},"hoverBackground1":{"background":1,"color":"#ffb0b0e6"},"border1":{"border":1,"radius":{"desktop":{"lock":true,"top":3,"right":3,"bottom":3,"left":3,"unit":"px"}},"size":{"desktop":{"lock":true}}}} -->
+								<div class="blockart-button blockart-button-d7d9f989 "><a class="blockart-button-link is-custom is-style-filled">HTML</a></div>
+								<!-- /wp:blockart/button-inner -->
+
+								<!-- wp:blockart/button-inner {"clientId":"d7d9f989","text":"CSS","size":"custom","padding":{"dimension":1,"desktop":{"lock":true,"top":10,"right":10,"bottom":10,"left":10,"unit":"px"}},"typography":{"typography":1,"size":{"desktop":{"value":14,"unit":"px"}}},"color1":"#1d1d1d","hoverColor1":"#1d1d1d","background1":{"background":1,"type":"color","color":"#ffb0b0e6"},"hoverBackground1":{"background":1,"color":"#ffb0b0e6"},"border1":{"border":1,"radius":{"desktop":{"lock":true,"top":3,"right":3,"bottom":3,"left":3,"unit":"px"}},"size":{"desktop":{"lock":true}}}} -->
+								<div class="blockart-button blockart-button-d7d9f989 "><a class="blockart-button-link is-custom is-style-filled">CSS</a></div>
+								<!-- /wp:blockart/button-inner -->
+
+								<!-- wp:blockart/button-inner {"clientId":"d7d9f989","text":"Vue.js","size":"custom","padding":{"dimension":1,"desktop":{"lock":true,"top":10,"right":10,"bottom":10,"left":10,"unit":"px"}},"typography":{"typography":1,"size":{"desktop":{"value":14,"unit":"px"}}},"color1":"#1d1d1d","hoverColor1":"#1d1d1d","background1":{"background":1,"type":"color","color":"#ffb0b0e6"},"hoverBackground1":{"background":1,"color":"#ffb0b0e6"},"border1":{"border":1,"radius":{"desktop":{"lock":true,"top":3,"right":3,"bottom":3,"left":3,"unit":"px"}},"size":{"desktop":{"lock":true}}}} -->
+								<div class="blockart-button blockart-button-d7d9f989 "><a class="blockart-button-link is-custom is-style-filled">Vue.js</a></div>
+								<!-- /wp:blockart/button-inner --></div>
+							<!-- /wp:blockart/button -->
+
+							<!-- wp:separator {"className":"is-style-wide"} -->
+							<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+							<!-- /wp:separator -->
+
+							<!-- wp:blockart/button {"clientId":"d186031b","blockMargin":{"dimension":1,"desktop":{"lock":true,"top":0,"right":0,"bottom":0,"left":0,"unit":"px"}}} -->
+							<div class="blockart-buttons blockart-buttons-d186031b is-wrap"><!-- wp:blockart/button-inner {"clientId":"81253b1c","text":"View project ","style":"plain","typography":{"typography":1,"size":{"desktop":{"value":16,"unit":"px"}}},"icon":{"enable":true,"icon":"arrowRight"},"iconSize":{"desktop":{"value":24,"unit":"px"}},"iconGap":{"desktop":{"value":2,"unit":"px"}},"color2":"#000000","blockMargin":{"dimension":1,"desktop":{"lock":false,"bottom":20,"unit":"px"}}} -->
+								<div class="blockart-button blockart-button-81253b1c "><a class="blockart-button-link is-large is-style-plain">View project <span class="blockart-button-icon"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="blockart-icon" aria-hidden="true"><path d="M21.8 12l-7-6v5H2v2h12.8v5l7-6z"></path></svg></span></a></div>
+								<!-- /wp:blockart/button-inner --></div>
+							<!-- /wp:blockart/button --></div>
+						<!-- /wp:group --></div></div>
+				<!-- /wp:blockart/column -->
+
+				<!-- wp:blockart/column {"clientId":"83b3b2c6","colWidth":{"desktop":33.34,"tablet":100,"mobile":100},"border":{"border":1,"radius":{"desktop":{"lock":true,"top":8,"right":8,"bottom":8,"left":8,"unit":"px"}},"size":{"desktop":{"lock":true,"top":1,"right":1,"bottom":1,"left":1,"unit":"px"}},"type":"none"},"boxShadow":{"boxShadow":1,"enable":true,"position":"outset","spread":-17,"blur":27,"horizontalX":0,"verticalY":4},"blockPadding":{"dimension":1,"desktop":{"lock":true,"unit":"px"}}} -->
+				<div class="blockart-column blockart-column-83b3b2c6"><div class="blockart-column-inner"><!-- wp:blockart/image {"clientId":"838a9a46","image":{"url":"http://place-hold.it/300","alt":""}} -->
+						<figure class="blockart-image blockart-image-838a9a46"><img class="wp-image-1360 hover-effect-static filter-none mask mask-none" src="http://place-hold.it/300" alt=""/></figure>
+						<!-- /wp:blockart/image -->
+
+						<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
+						<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:heading {"level":4,"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"0"}}}} -->
+							<h4 class="wp-block-heading" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:0">TMDB </h4>
+							<!-- /wp:heading -->
+
+							<!-- wp:paragraph -->
+							<p>A movie library</p>
+							<!-- /wp:paragraph -->
+
+							<!-- wp:blockart/button {"clientId":"bf448832"} -->
+							<div class="blockart-buttons blockart-buttons-bf448832 is-wrap"><!-- wp:blockart/button-inner {"clientId":"d7d9f989","text":"HTML","size":"custom","padding":{"dimension":1,"desktop":{"lock":true,"top":10,"right":10,"bottom":10,"left":10,"unit":"px"}},"typography":{"typography":1,"size":{"desktop":{"value":14,"unit":"px"}}},"color1":"#1d1d1d","hoverColor1":"#1d1d1d","background1":{"background":1,"type":"color","color":"#ffb0b0e6"},"hoverBackground1":{"background":1,"color":"#ffb0b0e6"},"border1":{"border":1,"radius":{"desktop":{"lock":true,"top":3,"right":3,"bottom":3,"left":3,"unit":"px"}},"size":{"desktop":{"lock":true}}}} -->
+								<div class="blockart-button blockart-button-d7d9f989 "><a class="blockart-button-link is-custom is-style-filled">HTML</a></div>
+								<!-- /wp:blockart/button-inner -->
+
+								<!-- wp:blockart/button-inner {"clientId":"d7d9f989","text":"CSS","size":"custom","padding":{"dimension":1,"desktop":{"lock":true,"top":10,"right":10,"bottom":10,"left":10,"unit":"px"}},"typography":{"typography":1,"size":{"desktop":{"value":14,"unit":"px"}}},"color1":"#1d1d1d","hoverColor1":"#1d1d1d","background1":{"background":1,"type":"color","color":"#ffb0b0e6"},"hoverBackground1":{"background":1,"color":"#ffb0b0e6"},"border1":{"border":1,"radius":{"desktop":{"lock":true,"top":3,"right":3,"bottom":3,"left":3,"unit":"px"}},"size":{"desktop":{"lock":true}}}} -->
+								<div class="blockart-button blockart-button-d7d9f989 "><a class="blockart-button-link is-custom is-style-filled">CSS</a></div>
+								<!-- /wp:blockart/button-inner -->
+
+								<!-- wp:blockart/button-inner {"clientId":"d7d9f989","text":"Vue.js","size":"custom","padding":{"dimension":1,"desktop":{"lock":true,"top":10,"right":10,"bottom":10,"left":10,"unit":"px"}},"typography":{"typography":1,"size":{"desktop":{"value":14,"unit":"px"}}},"color1":"#1d1d1d","hoverColor1":"#1d1d1d","background1":{"background":1,"type":"color","color":"#ffb0b0e6"},"hoverBackground1":{"background":1,"color":"#ffb0b0e6"},"border1":{"border":1,"radius":{"desktop":{"lock":true,"top":3,"right":3,"bottom":3,"left":3,"unit":"px"}},"size":{"desktop":{"lock":true}}}} -->
+								<div class="blockart-button blockart-button-d7d9f989 "><a class="blockart-button-link is-custom is-style-filled">Vue.js</a></div>
+								<!-- /wp:blockart/button-inner --></div>
+							<!-- /wp:blockart/button -->
+
+							<!-- wp:separator {"className":"is-style-wide"} -->
+							<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+							<!-- /wp:separator -->
+
+							<!-- wp:blockart/button {"clientId":"d186031b","blockMargin":{"dimension":1,"desktop":{"lock":true,"top":0,"right":0,"bottom":0,"left":0,"unit":"px"}}} -->
+							<div class="blockart-buttons blockart-buttons-d186031b is-wrap"><!-- wp:blockart/button-inner {"clientId":"81253b1c","text":"View project ","style":"plain","typography":{"typography":1,"size":{"desktop":{"value":16,"unit":"px"}}},"icon":{"enable":true,"icon":"arrowRight"},"iconSize":{"desktop":{"value":24,"unit":"px"}},"iconGap":{"desktop":{"value":2,"unit":"px"}},"color2":"#000000","blockMargin":{"dimension":1,"desktop":{"lock":false,"bottom":20,"unit":"px"}}} -->
+								<div class="blockart-button blockart-button-81253b1c "><a class="blockart-button-link is-large is-style-plain">View project <span class="blockart-button-icon"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="blockart-icon" aria-hidden="true"><path d="M21.8 12l-7-6v5H2v2h12.8v5l7-6z"></path></svg></span></a></div>
+								<!-- /wp:blockart/button-inner --></div>
+							<!-- /wp:blockart/button --></div>
+						<!-- /wp:group --></div></div>
+				<!-- /wp:blockart/column --></div></div></div>
+	<!-- /wp:blockart/section --></div>
+<!-- /wp:group -->
