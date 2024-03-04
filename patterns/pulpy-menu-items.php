f2d25902647b3df0dedee1c3a9d817b6969c1eb8
Kayla Nguyen <nnguyen1@my.wctc.edu>
2024-03-04 01:25:21 -0600

    Adjusted pulpy menu items to use BlockArt blocks

--- patterns/pulpy-menu-items.php
+++ patterns/pulpy-menu-items.php
@@ -8,8 +8,8 @@
  * https://developer.wordpress.org/themes/features/block-patterns/#registering-block-patterns
  */
 ?>
-<!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"0","right":"0"}}},"layout":{"type":"constrained","justifyContent":"left","contentSize":"100%","wideSize":"100%"},"metadata":{"name":"Pulpy Menu Items"}} -->
-<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--70);padding-right:0;padding-bottom:var(--wp--preset--spacing--70);padding-left:0"><!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"0","left":"var:preset|spacing|40"},"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}}} -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"90px","right":"90px"}}},"layout":{"type":"constrained","justifyContent":"left","contentSize":"100%","wideSize":"100%"},"metadata":{"name":"Pulpy Menu Items"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--70);padding-right:90px;padding-bottom:var(--wp--preset--spacing--70);padding-left:90px"><!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"0","left":"var:preset|spacing|40"},"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}}} -->
 	<div class="wp-block-columns" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:column {"width":"60%"} -->
 		<div class="wp-block-column" style="flex-basis:60%"><!-- wp:image {"id":1258,"sizeSlug":"full","linkDestination":"none","className":"is-resized"} -->
 			<figure class="wp-block-image size-full is-resized"><img src="http://localhost/wp-content/uploads/2024/02/menu-picture.jpeg" alt="" class="wp-image-1258"/></figure>
@@ -17,31 +17,33 @@
 		<!-- /wp:column -->
 
 		<!-- wp:column {"width":"40%","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"blockGap":"0"}}} -->
-		<div class="wp-block-column" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;flex-basis:40%"><!-- wp:heading {"style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"700","fontSize":"50px"}}} -->
-			<h2 class="wp-block-heading" style="font-size:50px;font-style:normal;font-weight:700;text-transform:uppercase">Homewrecker</h2>
-			<!-- /wp:heading -->
+		<div class="wp-block-column" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;flex-basis:40%"><!-- wp:blockart/heading {"clientId":"cc9b3351","text":"Homewrecker","color":"#494949","alignment":{"desktop":"left"},"typography":{"typography":1,"weight":"400","family":"Bebas Neue","size":{"desktop":{"value":50,"unit":"px"}},"transform":"uppercase"}} -->
+			<h2 class="blockart-heading blockart-heading-cc9b3351">Homewrecker</h2>
+			<!-- /wp:blockart/heading -->
 
-			<!-- wp:heading {"level":6,"style":{"typography":{"textTransform":"uppercase","fontSize":"20px"},"elements":{"link":{"color":{"text":"var:preset|color|vivid-red"}}}},"textColor":"vivid-red"} -->
-			<h6 class="wp-block-heading has-vivid-red-color has-text-color has-link-color" style="font-size:20px;text-transform:uppercase">Mexican burrito with guac</h6>
-			<!-- /wp:heading -->
+			<!-- wp:blockart/heading {"clientId":"f63ba0f1","markup":"h6","text":"Mexican burrito with guac","color":"#bd3224","alignment":{"desktop":"left"},"typography":{"typography":1,"weight":"700","family":"Montserrat","size":{"desktop":{"value":20,"unit":"px"}},"transform":"uppercase","letterSpacing":{"desktop":{"value":0,"unit":"px"}}}} -->
+			<h6 class="blockart-heading blockart-heading-f63ba0f1">Mexican burrito with guac</h6>
+			<!-- /wp:blockart/heading -->
 
-			<!-- wp:paragraph {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"elements":{"link":{"color":{"text":"var:preset|color|black"}}},"typography":{"fontSize":"16px"}},"textColor":"black"} -->
-			<p class="has-black-color has-text-color has-link-color" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);font-size:16px">It's big. It's legendary. Our Homewrecker comes with your choice of protein, rice, beans, shredded cheese, shredded letuce, pico de gallo, sour cream and guac. And yes, guac is included.</p>
-			<!-- /wp:paragraph -->
+			<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"constrained","justifyContent":"left"},"metadata":{"name":"Food Meta"}} -->
+			<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:blockart/paragraph {"clientId":"624db15f","text":"It's big. It's legendary. Our Homewrecker comes with your choice of protein, rice, beans, shredded cheese, shredded letuce, pico de gallo, sour cream and guac. And yes, guac is included.","alignment":{"desktop":"left"},"typography":{"typography":1,"family":"Montserrat","size":{"desktop":{"value":16,"unit":"px"}}},"color":"#494949"} -->
+				<p class="blockart-paragraph blockart-paragraph-624db15f">It's big. It's legendary. Our Homewrecker comes with your choice of protein, rice, beans, shredded cheese, shredded letuce, pico de gallo, sour cream and guac. And yes, guac is included.</p>
+				<!-- /wp:blockart/paragraph -->
 
-			<!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"600","letterSpacing":"0.5px","fontSize":"16px"}}} -->
-			<p style="font-size:16px;font-style:normal;font-weight:600;letter-spacing:0.5px;text-transform:uppercase">Protein choiceS:</p>
-			<!-- /wp:paragraph -->
+				<!-- wp:blockart/paragraph {"clientId":"a45d97bf","text":"Protein Choices:","alignment":{"desktop":"left"},"typography":{"typography":1,"family":"Montserrat","size":{"desktop":{"value":16,"unit":"px"}},"transform":"uppercase","weight":"700"},"color":"#494949","blockMargin":{"dimension":1,"desktop":{"lock":true,"top":0,"right":0,"bottom":0,"left":0,"unit":"px"}},"blockPadding":{"dimension":1,"desktop":{"lock":false,"bottom":8,"unit":"px"}}} -->
+				<p class="blockart-paragraph blockart-paragraph-a45d97bf">Protein Choices:</p>
+				<!-- /wp:blockart/paragraph -->
 
-			<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600","letterSpacing":"0.5px","fontSize":"16px"},"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"},"margin":{"bottom":"var:preset|spacing|60"}}}} -->
-			<p style="margin-bottom:var(--wp--preset--spacing--60);padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);font-size:16px;font-style:normal;font-weight:600;letter-spacing:0.5px">Sirloin Steak, Natural White Meat Chicken, Adobo Chicken, Ground Beef or organic Tofu.</p>
-			<!-- /wp:paragraph -->
+				<!-- wp:blockart/paragraph {"clientId":"a43fb5c5","text":"Sirloin Steak, Natural White Meat Chicken, Adobo Chicken, Ground Beef or organic Tofu.","alignment":{"desktop":"left"},"typography":{"typography":1,"family":"Montserrat","size":{"desktop":{"value":16,"unit":"px"}},"transform":"default","weight":"700"},"color":"#494949"} -->
+				<p class="blockart-paragraph blockart-paragraph-a43fb5c5">Sirloin Steak, Natural White Meat Chicken, Adobo Chicken, Ground Beef or organic Tofu.</p>
+				<!-- /wp:blockart/paragraph --></div>
+			<!-- /wp:group -->
 
-			<!-- wp:buttons -->
-			<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"vivid-red","style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"100","fontSize":"22px"},"spacing":{"padding":{"left":"var:preset|spacing|40","right":"var:preset|spacing|40","top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}}} -->
-				<div class="wp-block-button has-custom-font-size" style="font-size:22px;font-style:normal;font-weight:100;text-transform:uppercase"><a class="wp-block-button__link has-vivid-red-background-color has-background wp-element-button" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--40)"><strong>Order now</strong></a></div>
-				<!-- /wp:button --></div>
-			<!-- /wp:buttons --></div>
+			<!-- wp:blockart/button {"clientId":"35192971"} -->
+			<div class="blockart-buttons blockart-buttons-35192971 is-wrap"><!-- wp:blockart/button-inner {"clientId":"7327bfb6","text":"Order Now","typography":{"typography":1,"family":"Bebas Neue","size":{"desktop":{"value":22,"unit":"px"}},"letterSpacing":{"desktop":{"value":1,"unit":"px"}}},"icon":{"enable":true,"icon":"chevronRight"},"iconSize":{"desktop":{"value":20,"unit":"px"}},"iconGap":{"desktop":{"value":5,"unit":"px"}},"hoverColor1":"#a53422","background1":{"background":1,"type":"color","color":"#a53422"},"hoverBackground1":{"background":1,"color":"#ffffff"},"hoverBorder1":{"border":1,"radius":{"desktop":{"lock":true}},"size":{"desktop":{"lock":true,"top":2,"right":2,"bottom":2,"left":2,"unit":"px"}},"type":"solid","color":"#a53422"},"blockMargin":{"dimension":1,"desktop":{"lock":false,"top":15,"unit":"px"}}} -->
+				<div class="blockart-button blockart-button-7327bfb6 "><a class="blockart-button-link is-large is-style-filled">Order Now<span class="blockart-button-icon"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="blockart-icon" aria-hidden="true"><path d="M6.6 3.4l8.7 8.6-8.7 8.7L8 22.1 18 12 8 2 6.6 3.4z"></path></svg></span></a></div>
+				<!-- /wp:blockart/button-inner --></div>
+			<!-- /wp:blockart/button --></div>
 		<!-- /wp:column --></div>
 	<!-- /wp:columns --></div>
 <!-- /wp:group -->
