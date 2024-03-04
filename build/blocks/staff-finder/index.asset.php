1d7d44d3ed92be46c5118d05c4447d6ebfc03bc8
Kayla Nguyen <nnguyen1@my.wctc.edu>
2024-03-04 09:27:15 -0600

    Initial commit for staff finder block

---
+++ build/blocks/staff-finder/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '7541331448a6a26fce52');
---
+++ build/blocks/staff-finder/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '658be78fa2afc2210bb8');
